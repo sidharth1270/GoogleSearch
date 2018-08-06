3b350f51f5d57020b6107ac2d7f3153af104c64b
sidharth1270 <42133242+sidharth1270@users.noreply.github.com>
2018-08-06 12:53:05 +0530

    Added missing package

--- RunTestNunit.bat
+++ RunTestNunit.bat
@@ -1,1 +1,1 @@
-packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe GoogleSearch\bin\debug\GoogleSearch.dll --out=TestResult.txt --result="TestResult.xml" 
+package\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe GoogleSearch\bin\debug\GoogleSearch.dll --out=TestResult.txt --result="TestResult.xml" 
